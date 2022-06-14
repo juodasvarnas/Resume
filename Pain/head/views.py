0dde504ca7818f6c4c1f586a723ef35765ffc744
juodasvarnas <101868391+juodasvarnas@users.noreply.github.com>
2022-06-14 14:54:18 +0300

    yes

--- Pain/head/views.py
+++ Pain/head/views.py
@@ -2,9 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Pain
+
 
 def index(request):
-    return HttpResponse('<h1>Вы находитесь на главной странице, это значит,что джанго работает!</h1>')
+    pain = Pain.objects.all()
+    res = '<h1>На должность представляем</h1>'
+    for item in pain:
+         res+= f'<div>\n<p>{item.Name}</p>\n<p>{item.photo}</p>\n</div>\n<hr>\n'
+    return HttpResponse(res)
 
 def test(request):
     return HttpResponse('Тестовая страница')
